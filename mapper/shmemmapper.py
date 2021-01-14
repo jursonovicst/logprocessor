aa14c081337da803731c54e2c6e342a9585ecbbb
jursonovicst <github.com@jursonovics.com>
2021-01-14 22:25:30 +0100

    no need

--- mapper/shmemmapper.py
+++
@@ -1,8 +0,0 @@
-from mapper import BaseMapper
-from multiprocessing import Manager
-
-
-class SHMemMapper(BaseMapper):
-
-    def __init__(self, hashlen: int, prefix: str = '', maxcachesize: int = 10000):
-        super().__init__(hashlen, prefix, Manager().dict(), maxcachesize)
