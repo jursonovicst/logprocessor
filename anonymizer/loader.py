486013167b87a7a38c18a689cad44ae7a2bf7386
jursonovicst <github.com@jursonovics.com>
2021-01-14 14:38:43 +0100

    version 1

---
+++ anonymizer/loader.py
@@ -0,0 +1,281 @@
+import pandas as pd
+import multiprocessing as mp
+from tqdm.auto import tqdm
+from mapper import Client
+from bz2 import BZ2File
+import numpy as np
+from cachetools import cached
+from ua_parser import user_agent_parser
+from pandas import HDFStore
+import os
+from contextlib import nullcontext
+from datetime import timedelta
+from geolite2 import geolite2
+from urllib.parse import urlsplit
+
+
+class Loader:
+    # constant values
+    _cachename = ''
+    _popname = ''
+
+    # secrets
+    timeshiftdays = 0
+    xyte = 0
+
+    # local caches for acceleration
+    _cachesize = 0
+    _geocache = None
+    _uacache = None
+
+    @classmethod
+    def initializer(cls, cachename: str, popname: str, cachesize: int, timeshiftdays: int, xyte: float):
+        assert isinstance(cachename, str) and len(cachename) > 0, f"invalid cachename: '{cachename}'"
+        assert isinstance(popname, str) and len(popname) > 0, f"invalid cachename: '{popname}'"
+        cls._cachename = cachename
+        cls._popname = popname
+
+        assert cachesize >= 0, f"Wrong cachesize: {cachesize}"
+        cls._cachesize = int(cachesize)
+        cls._geocache = {}
+        cls._uacache = {}
+
+        assert timeshiftdays > 0, f"invalid timeshiftdays: '{timeshiftdays}'"
+        cls.timeshiftdays = int(timeshiftdays)
+        cls.xyte = float(xyte)
+
+    @classmethod
+    def process(cls, chunk: pd.DataFrame):
+
+        _debug = False
+
+        if _debug:
+            pd.set_option('display.max_columns', None)
+            pd.set_option('display.max_colwidth', -1)
+
+        #        try:
+        #########################
+        # check TODO: add missing
+        if 'ip' not in chunk.columns \
+                or 'xforwardedfor' not in chunk.columns \
+                or 'timestamp' not in chunk.columns \
+                or 'contenttype' not in chunk.columns \
+                or 'ip' not in chunk.columns \
+                or 'request' not in chunk.columns \
+                or 'side' not in chunk.columns \
+                or 'statuscode' not in chunk.columns \
+                or 'timetoserv' not in chunk.columns \
+                :
+            raise SyntaxError(f"Required column(s) not found: {chunk.columns}")
+
+        if chunk.ip.dtype != object or \
+                chunk.xforwardedfor.dtype != object or \
+                chunk.timetoserv.dtype != float or \
+                chunk.statuscode.dtype != np.int64:
+            raise SyntaxError(f"dtype(s) incorrect: {chunk.dtypes}")
+
+        if _debug: print(chunk.head(5))
+
+        #########################
+        # filter
+
+        # drop non downstream lines
+        chunk.drop(chunk.loc[chunk['side'] != 'c'].index, inplace=True)
+        chunk.drop(['side'], axis=1, inplace=True)
+
+        # add constant values
+        chunk['cachename'] = cls._cachename
+        chunk['popname'] = cls._popname
+
+        #########################
+        # parse
+
+        # split xforwarded for, keep the first IP
+        chunk.xforwardedfor = chunk.xforwardedfor.str.split(",", n=1, expand=True)[0]
+
+        # check if all public
+        assert True  # TODO: implement
+        if _debug: print(chunk.head(5))
+
+        # overwrite ip with xforwardedfor if it is 127.0.0.1 (TLS termination is from localhost)
+        mask = chunk['ip'] == '127.0.0.1'
+        chunk.loc[mask, 'ip'] = chunk.loc[mask, 'xforwardedfor']
+        if _debug: print(chunk.head(5))
+
+        # drop xforwardedfor
+        chunk.drop(['xforwardedfor'], axis=1, inplace=True)
+        if _debug: print(chunk.head(5))
+
+        # convert timetoserv unit from ms to sec
+        chunk['timetoserv'] /= 1000000
+
+        # split request line
+        chunk['method'], chunk['url'], chunk['protocol'] = zip(*chunk['request'].str.split(' ', n=2))
+        chunk.drop(['request'], axis=1, inplace=True)
+
+        # parse url, skip schema, fragment
+        dummy, chunk['host'], chunk['path'], chunk['query'], dummy2 = zip(*chunk['url'].map(urlsplit))
+        chunk.drop(['url'], axis=1, inplace=True)
+
+        #########################
+        # enrich - geoip, use local cache for performance
+        geo = geolite2.reader()
+
+        @cached(cache=cls._geocache)
+        def coord(ip: str) -> str:
+            geodata = geo.get(ip)
+            return f"{geodata['location']['longitude']}:{geodata['location']['latitude']}" if geodata is not None else np.nan
+
+        chunk['coordinates'] = chunk['ip'].map(coord, na_action='ignore')
+        chunk.drop(['ip'], axis=1, inplace=True)
+
+        if _debug: print(chunk.head(5))
+
+        #########################
+        # enrich - user agent, use local cache for performance
+
+        @cached(cache=cls._uacache)
+        def uaparser(ua_string: str) -> pd.Series:
+            ps = user_agent_parser.Parse(ua_string)
+            return pd.Series(
+                [np.nan if x is None else x for x in
+                 [ps['device']['brand'], ps['device']['family'], ps['device']['model'], ps['os']['family'],
+                  ps['user_agent']['family'], ps['user_agent']['major']]]
+            )
+
+        chunk[["devicebrand", "devicefamily", "devicemodel", "osfamily", "uafamily", "uamajor"]] = chunk.loc[
+            chunk['useragent'].notna(), 'useragent'].apply(uaparser)
+        chunk.drop(['useragent'], axis=1, inplace=True)
+
+        if _debug: print(chunk.head(5))
+
+        #########################
+        # anonymize
+
+        # create mapper client and substitute
+        client = Client(cls._cachesize, prefix='coordinates', hashlen=8)
+        chunk['contenttype'] = chunk['contenttype'].map(client.get, na_action='ignore')
+        client.prefix = 'devicebrand'
+        client.prefix = 'cachename'
+        chunk['cachename'] = chunk['cachename'].map(client.get, na_action='ignore')
+        client.prefix = 'popname'
+        chunk['popname'] = chunk['popname'].map(client.get, na_action='ignore')
+        client.prefix = 'host'
+        chunk['host'] = chunk['host'].map(client.get, na_action='ignore')
+        chunk['coordinates'] = chunk['coordinates'].map(client.get, na_action='ignore')
+        client.prefix = 'contenttype'
+        chunk['devicebrand'] = chunk['devicebrand'].map(client.get, na_action='ignore')
+        client.prefix = 'devicefamily'
+        chunk['devicefamily'] = chunk['devicefamily'].map(client.get, na_action='ignore')
+        client.prefix = 'devicemodel'
+        chunk['devicemodel'] = chunk['devicemodel'].map(client.get, na_action='ignore')
+        client.prefix = 'osfamily'
+        chunk['osfamily'] = chunk['osfamily'].map(client.get, na_action='ignore')
+        client.prefix = 'uafamily'
+        chunk['uafamily'] = chunk['uafamily'].map(client.get, na_action='ignore')
+        client.prefix = 'uamajor'
+        chunk['uamajor'] = chunk['uamajor'].map(client.get, na_action='ignore')
+
+        client.prefix = 'path'
+        client.hashlen = 20
+        chunk['path'] = chunk['path'].map(client.get, na_action='ignore')
+        client.prefix = 'query'
+        chunk['query'] = chunk['query'].map(client.get, na_action='ignore')
+
+        if _debug: print(chunk.head(5))
+
+        # shift time
+        chunk.timestamp += timedelta(days=cls.timeshiftdays)
+
+        # convert byte to xyte
+        chunk.contentlength = chunk.contentlength.divide(cls.xyte)
+
+        #########################
+        # set index
+        chunk.set_index(['timestamp', 'statuscode', 'method', 'protocol',
+                         'hit', 'contenttype', 'cachename', 'popname', 'host', 'coordinates', 'devicebrand',
+                         'devicefamily', 'devicemodel', 'osfamily', 'uafamily', 'uamajor', 'path', 'query'],
+                        inplace=True)
+
+        assert set(chunk.columns) == set(
+            list(['contentlength', 'timefirstbyte', 'timetoserv'])), f"Wrong column names: {chunk.columns}"
+        # returns
+        return chunk
+
+    #        except Exception as e:
+    #            return e
+
+    def __init__(self, nproc: int, cachesize: int, timeshiftdays: int, xyte: float):
+        # worker parameters
+        self._nproc = nproc
+        self._cachesize = cachesize
+
+        # secrets
+        self.timeshiftdays = timeshiftdays
+        self.xyte = xyte
+
+    def load(self, logfilename: str, cachename: str, popname: str, exportcsv=True, **read_csv_args):
+
+        # storage
+        if exportcsv:
+            storecm = nullcontext()
+        else:
+            storecm = HDFStore(f"{logfilename}.hd5", mode='w')
+
+        # start worker processes with initializer (worker parameters and secrets)
+        # open raw logfile
+        # create progress bar for file position
+        # create progress bar for processed lines
+        # create decompressor
+        # create store (if needed)
+        with mp.Pool(self._nproc, Loader.initializer,
+                     (cachename, popname, self._cachesize, self.timeshiftdays, self.xyte,)) as pool, \
+                open(logfilename, 'rb') as logfile, \
+                tqdm(total=os.path.getsize(logfilename), position=0, desc=logfilename, unit='B',
+                     unit_scale=True) as pbar_filepos, \
+                tqdm(position=1, unit='line', desc='lines', unit_scale=True) as pbar_lines, \
+                BZ2File(logfile) as reader, \
+                storecm as store:
+
+            # for progress bar
+            lastpos = 0
+
+            # open logfile with pandas, use chunks to distribute the load among workers. Force dtypes
+            chunk_reader = pd.read_csv(reader, **read_csv_args,
+                                       dtype={
+                                           'ip': object,
+                                           'timefirstbyte': float,
+                                           'timetoserv': float
+                                       }
+                                       , iterator=True)  # TODO: add dtypes
+
+            # map the logprocessor function
+            for result in pool.imap_unordered(Loader.process, chunk_reader):
+
+                # update progress bar
+                if logfile.tell() > lastpos:
+                    pbar_filepos.update(logfile.tell() - lastpos)
+                    lastpos = logfile.tell()
+
+                # process result
+                if isinstance(result, pd.DataFrame):
+                    if exportcsv:
+                        result.to_csv(f"{logfilename}.csv", mode='a', header=True)
+                    else:
+                        store.append('logs', result)
+
+                    # update progress bar
+                    pbar_lines.update(result.shape[0])
+
+                elif isinstance(result, KeyboardInterrupt):
+                    # exit for loop
+                    break
+
+                elif isinstance(result, Exception):
+                    pbar_filepos.display(f"{type(result)} processing chunk: '{result}'")
+
+                elif isinstance(result, str):
+                    pbar_filepos.display(f"Message: {result}")
+
+                else:
+                    pbar_filepos.display(f"Unknown result type: '{type(result)}'")
