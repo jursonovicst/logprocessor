aeee0c66f7fbb8b4617a08a0680c13a4da4c25ab
jursonovicst <github.com@jursonovics.com>
2021-01-14 21:42:26 +0100

    fix

--- anonymizer/loader.py
+++ anonymizer/loader.py
@@ -58,133 +58,135 @@ class Loader:
             pd.set_option('display.max_columns', None)
             pd.set_option('display.max_colwidth', -1)
 
-        #        try:
-        #########################
-        # check TODO: add missing
-        if 'ip' not in chunk.columns \
-                or 'xforwardedfor' not in chunk.columns \
-                or 'timestamp' not in chunk.columns \
-                or 'contenttype' not in chunk.columns \
-                or 'ip' not in chunk.columns \
-                or 'request' not in chunk.columns \
-                or 'side' not in chunk.columns \
-                or 'statuscode' not in chunk.columns \
-                or 'timetoserv' not in chunk.columns \
-                :
-            raise SyntaxError(f"Required column(s) not found: {chunk.columns}")
+        try:
+            #########################
+            # check TODO: add missing
+            if 'ip' not in chunk.columns \
+                    or 'xforwardedfor' not in chunk.columns \
+                    or 'timestamp' not in chunk.columns \
+                    or 'contenttype' not in chunk.columns \
+                    or 'ip' not in chunk.columns \
+                    or 'request' not in chunk.columns \
+                    or 'side' not in chunk.columns \
+                    or 'statuscode' not in chunk.columns \
+                    or 'timetoserv' not in chunk.columns \
+                    :
+                raise SyntaxError(f"Required column(s) not found: {chunk.columns}")
 
-        if chunk.ip.dtype != object or \
-                chunk.xforwardedfor.dtype != object or \
-                chunk.timetoserv.dtype != float or \
-                chunk.statuscode.dtype != np.int64:
-            raise SyntaxError(f"dtype(s) incorrect: {chunk.dtypes}")
+            if chunk.ip.dtype != object or \
+                    chunk.xforwardedfor.dtype != object or \
+                    chunk.timetoserv.dtype != float or \
+                    chunk.statuscode.dtype != np.int64:
+                raise SyntaxError(f"dtype(s) incorrect: {chunk.dtypes}")
 
-        if _debug: print(chunk.head(5))
+            if _debug: print(chunk.head(5))
 
-        #########################
-        # filter
+            #########################
+            # filter
 
-        # drop non downstream lines
-        chunk.drop(chunk.loc[chunk['side'] != 'c'].index, inplace=True)
-        chunk.drop(['side'], axis=1, inplace=True)
+            # drop non downstream lines
+            chunk.drop(chunk.loc[chunk['side'] != 'c'].index, inplace=True)
+            chunk.drop(['side'], axis=1, inplace=True)
 
-        # add constant values
-        chunk['cachename'] = cls._cachename
-        chunk['popname'] = cls._popname
+            # add constant values
+            chunk['cachename'] = cls._cachename
+            chunk['popname'] = cls._popname
 
-        #########################
-        # parse
+            #########################
+            # parse
 
-        # split xforwarded for, keep the first IP
-        chunk.xforwardedfor = chunk.xforwardedfor.str.split(",", n=1, expand=True)[0]
+            # split xforwarded for, keep the first IP
+            chunk.xforwardedfor = chunk.xforwardedfor.str.split(",", n=1, expand=True)[0]
 
-        # check if all public
-        assert True  # TODO: implement
-        if _debug: print(chunk.head(5))
+            # check if all public
+            assert True  # TODO: implement
+            if _debug: print(chunk.head(5))
 
-        # overwrite ip with xforwardedfor if it is 127.0.0.1 (TLS termination is from localhost)
-        mask = chunk['ip'] == '127.0.0.1'
-        chunk.loc[mask, 'ip'] = chunk.loc[mask, 'xforwardedfor']
-        if _debug: print(chunk.head(5))
+            # overwrite ip with xforwardedfor if it is 127.0.0.1 (TLS termination is from localhost)
+            mask = chunk['ip'] == '127.0.0.1'
+            chunk.loc[mask, 'ip'] = chunk.loc[mask, 'xforwardedfor']
+            if _debug: print(chunk.head(5))
 
-        # drop xforwardedfor
-        chunk.drop(['xforwardedfor'], axis=1, inplace=True)
-        if _debug: print(chunk.head(5))
+            # drop xforwardedfor
+            chunk.drop(['xforwardedfor'], axis=1, inplace=True)
+            if _debug: print(chunk.head(5))
 
-        # convert timetoserv unit from ms to sec
-        chunk['timetoserv'] /= 1000000
+            # convert timetoserv unit from ms to sec
+            chunk['timetoserv'] /= 1000000
 
-        # split request line
-        chunk['method'], chunk['url'], chunk['protocol'] = zip(*chunk['request'].str.split(' ', n=2))
-        chunk.drop(['request'], axis=1, inplace=True)
+            # split request line
+            chunk['method'], chunk['url'], chunk['protocol'] = zip(*chunk['request'].str.split(' ', n=2))
+            chunk.drop(['request'], axis=1, inplace=True)
 
-        # parse url, skip schema, fragment
-        dummy, chunk['host'], chunk['path'], chunk['query'], dummy2 = zip(*chunk['url'].map(urlsplit))
-        chunk.drop(['url'], axis=1, inplace=True)
+            # parse url, skip schema, fragment
+            dummy, chunk['host'], chunk['path'], chunk['query'], dummy2 = zip(*chunk['url'].map(urlsplit))
+            chunk.drop(['url'], axis=1, inplace=True)
 
-        #########################
-        # enrich - geoip, use local cache for performance
-        geo = geolite2.reader()
+            #########################
+            # enrich - geoip, use local cache for performance
+            geo = geolite2.reader()
 
-        @cached(cache=cls._geocache)
-        def coord(ip: str) -> str:
-            geodata = geo.get(ip)
-            return f"{geodata['location']['longitude']}:{geodata['location']['latitude']}" if geodata is not None and 'location' in geodata else np.nan
+            @cached(cache=cls._geocache)
+            def coord(ip: str) -> str:
+                geodata = geo.get(ip)
+                return f"{geodata['location']['longitude']}:{geodata['location']['latitude']}" if geodata is not None and 'location' in geodata else np.nan
 
-        chunk['coordinates'] = chunk['ip'].map(coord, na_action='ignore')
-        chunk.drop(['ip'], axis=1, inplace=True)
+            chunk['coordinates'] = chunk['ip'].map(coord, na_action='ignore')
+            chunk.drop(['ip'], axis=1, inplace=True)
 
-        if _debug: print(chunk.head(5))
+            if _debug: print(chunk.head(5))
 
-        #########################
-        # enrich - user agent, use local cache for performance
+            #########################
+            # enrich - user agent, use local cache for performance
 
-        @cached(cache=cls._uacache)
-        def uaparser(ua_string: str) -> pd.Series:
-            ps = user_agent_parser.Parse(ua_string)
-            return pd.Series(
-                [np.nan if x is None else x for x in
-                 [ps['device']['brand'], ps['device']['family'], ps['device']['model'], ps['os']['family'],
-                  ps['user_agent']['family'], ps['user_agent']['major']]]
-            )
+            @cached(cache=cls._uacache)
+            def uaparser(ua_string: str) -> pd.Series:
+                ps = user_agent_parser.Parse(ua_string)
+                return pd.Series(
+                    [np.nan if x is None else x for x in
+                     [ps['device']['brand'], ps['device']['family'], ps['device']['model'], ps['os']['family'],
+                      ps['user_agent']['family'], ps['user_agent']['major']]]
+                )
 
-        chunk[["devicebrand", "devicefamily", "devicemodel", "osfamily", "uafamily", "uamajor"]] = chunk.loc[
-            chunk['useragent'].notna(), 'useragent'].apply(uaparser)
-        chunk.drop(['useragent'], axis=1, inplace=True)
+            chunk[["devicebrand", "devicefamily", "devicemodel", "osfamily", "uafamily", "uamajor"]] = chunk.loc[
+                chunk['useragent'].notna(), 'useragent'].apply(uaparser)
+            chunk.drop(['useragent'], axis=1, inplace=True)
 
-        if _debug: print(chunk.head(5))
+            if _debug: print(chunk.head(5))
 
-        #########################
-        # anonymize
+            #########################
+            # anonymize
 
-        # substitute: map values to random hashes
-        for prefix in ['contenttype', 'cachename', 'popname', 'host', 'coordinates', 'devicebrand',
-                       'devicefamily', 'devicemodel', 'osfamily', 'uafamily', 'uamajor', 'path', 'query']:
-            assert prefix in Loader.mappers, f"Mapper prefix issue: '{prefix}' not found in '{Loader.mappers}'"
-            chunk[prefix] = chunk[prefix].map(Loader.mappers[prefix].get, na_action='ignore')
+            # substitute: map values to random hashes
+            for prefix in ['contenttype', 'cachename', 'popname', 'host', 'coordinates', 'devicebrand',
+                           'devicefamily', 'devicemodel', 'osfamily', 'uafamily', 'uamajor', 'path', 'query']:
+                assert prefix in Loader.mappers, f"Mapper prefix issue: '{prefix}' not found in '{Loader.mappers}'"
+                chunk[prefix] = chunk[prefix].map(Loader.mappers[prefix].get, na_action='ignore')
 
-        if _debug: print(chunk.head(5))
+            if _debug: print(chunk.head(5))
 
-        # shift time
-        chunk.timestamp += timedelta(days=cls.timeshiftdays)
+            # shift time
+            chunk.timestamp += timedelta(days=cls.timeshiftdays)
 
-        # convert byte to xyte
-        chunk.contentlength = chunk.contentlength.divide(cls.xyte)
+            # convert byte to xyte
+            chunk.contentlength = chunk.contentlength.divide(cls.xyte)
 
-        #########################
-        # set index
-        chunk.set_index(['timestamp', 'statuscode', 'method', 'protocol',
-                         'hit', 'contenttype', 'cachename', 'popname', 'host', 'coordinates', 'devicebrand',
-                         'devicefamily', 'devicemodel', 'osfamily', 'uafamily', 'uamajor', 'path', 'query'],
-                        inplace=True)
+            #########################
+            # set index
+            chunk.set_index(['timestamp', 'statuscode', 'method', 'protocol',
+                             'hit', 'contenttype', 'cachename', 'popname', 'host', 'coordinates', 'devicebrand',
+                             'devicefamily', 'devicemodel', 'osfamily', 'uafamily', 'uamajor', 'path', 'query'],
+                            inplace=True)
 
-        assert set(chunk.columns) == set(
-            list(['contentlength', 'timefirstbyte', 'timetoserv'])), f"Wrong column names: {chunk.columns}"
-        # returns
-        return chunk
+            assert set(chunk.columns) == set(
+                list(['contentlength', 'timefirstbyte',
+                      'timetoserv'])), f"Somethink went wrong, column name mismatch: {chunk.columns}"
 
-    #        except Exception as e:
-    #            return e
+            # return
+            return chunk
+
+        except Exception as e:
+            return e
 
     def __init__(self, nproc: int, cachesize: int, timeshiftdays: int, xyte: float):
         # worker parameters
